3622b49273d2f07ad304f3c5c85baf41e0b0f1e0
Simon Weis <laguna@runvs.de>
2015-12-16 05:10:50 -0500

    improvement of the point distribution

--- test/2015-12-14_input_test/read.lua
+++ test/2015-12-14_input_test/read.lua
@@ -12,11 +12,11 @@ function docalculation (p)
 
     for i=0,stepstheta,1 do 
         for j=0,stepsphi,1 do 
-            local theta = i * (1.0/stepstheta)* math.pi
-            local phi   = j *  (1.0/stepsphi)* math.pi * 2. - math.pi
-            local x = xoffset + math.sin(theta)*math.cos(phi)*r
+            local theta = i * (1.0/stepstheta)* math.pi * 2.0
+            local phi = math.acos(j *  (2.0/stepsphi) - 1.0)
+            local x = xoffset + math.cos(theta)*math.sin(phi)*r
             local y = yoffset + math.sin(theta)*math.sin(phi)*r
-            local z = zoffset + math.cos(theta)*r
+            local z = zoffset + math.cos(phi)*r
             p:addpoint(x, y, z, l)
         end
     end
