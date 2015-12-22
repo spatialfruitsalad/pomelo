b6d22e91902f924254c2276a02689221152c1a64
Simon <laguna@runvs.de>
2015-12-22 15:19:31 +0100

    adding another test

---
+++ test/2015-12-22_spheres_center/read.lua
@@ -0,0 +1,13 @@
+stepstheta = 14 
+stepsphi = 14
+shrink = 0.4
+
+s = {}
+
+function docalculation (p) 
+    local l = s[0] 
+    local xoffset = s[1]
+    local yoffset = s[2]
+    local zoffset = s[3]
+    p:addpoint(xoffset, yoffset, zoffset, l)
+end
---
+++ test/2015-12-22_spheres_center/spheresCenter.lua
@@ -0,0 +1,12 @@
+positionfile = "/home/tester/setvoronoi/test/2015-12-22_spheres_center/ellipsoids_linear_std5.ellip"
+readfile = "/home/tester/setvoronoi/test/2015-12-22_spheres_center/read.lua"
+
+xmin = 0
+ymin = 0
+zmin = 0
+
+xmax = 1132
+ymax = 1132
+zmax = 1000
+
+epsilon = 1e-6
